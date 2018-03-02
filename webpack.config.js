93420c4e09da2e8505f1df6b475a6e0833399de1
yoshihara <yoshihara@users.noreply.github.com>
2018-03-02 10:19:08 +0900

    Use babel w/ babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
   // plugins: [
   //   new webpack.ProvidePlugin({
